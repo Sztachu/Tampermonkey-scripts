50c0cfa31c68925770206aa13a7bfa97ea072d00
Sztachu <Sztachu@users.noreply.github.com>
2019-10-16 10:50:09 +0200

    Hide elements via css

--- wykop-ad-remover.js
+++ wykop-ad-remover.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         WykopAdRemover
 // @namespace    https://github.com/Sztachu/Tampermonkey-scripts
-// @version      0.21
+// @version      0.22
 // @description  wykop.pl - ad remover
 // @author       Sztachu
 // @match        https://www.wykop.pl/*
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    document.querySelectorAll('#itemsStream li.link.iC > div:not(.dC)').forEach((el)=>el.remove());        
-    document.querySelectorAll('#itemsStream li.link.iC > div.empty-media').forEach((el)=>el.remove());        
-    document.querySelector('#dyingLinksBox').remove();    
+    GM_addStyle(' #dyingLinksBox, #itemsStream li.link.iC > div:not(.dC), #itemsStream li.link.iC > div.empty-media { display: none !important; } ');
+    
+    // document.querySelectorAll('#itemsStream li.link.iC > div:not(.dC)').forEach((el)=>el.remove());        
+    // document.querySelectorAll('#itemsStream li.link.iC > div.empty-media').forEach((el)=>el.remove());        
+    // document.querySelector('#dyingLinksBox').remove();    
 })();
